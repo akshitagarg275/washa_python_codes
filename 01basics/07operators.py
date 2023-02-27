d0a144d800546bd96fc9b5d939b85b18f3833793
akshita garg <akshitagarg275@gmail.com>
2023-02-27 10:13:57 +0530

    arithmetic operators

---
+++ 01basics/07operators.py
@@ -0,0 +1,41 @@
+'''
+operators
+
+unary -> In which one operand in required
+binary -> In which two operands are required
+
+operand operator operand
+
+arithmetic operators
+
+'''
+
+# TODO: Unary
+# a = -1
+
+# TODO: Arithmetic
+'''
++, - , / , *
+
+// -> floor divison
+It provides integer quotient
+
+% -> Modulus operator
+It gives us remainder
+
+** -> exponentation operator
+'''
+
+num1 = 5
+num2 = 2
+
+# print("Add is :", num1 + num2)
+# print("Sub is :", num1 - num2)
+# print("Mul is :", num1 * num2)
+# print("Div is :", num1 / num2)
+
+# print("Integer quotient is: ",num1 // num2)
+# print("Remainder is: ",num1 % num2)
+
+print(5 ** 2)
+print(3 ** 3)
