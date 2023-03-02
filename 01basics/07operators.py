79ef77d4b89f327f55d9e0b9caa0dcb15b8dcb9f
akshita garg <akshitagarg275@gmail.com>
2023-03-02 22:44:07 +0530

    operators

--- 01basics/07operators.py
+++ 01basics/07operators.py
@@ -37,5 +37,69 @@ num2 = 2
 # print("Integer quotient is: ",num1 // num2)
 # print("Remainder is: ",num1 % num2)
 
-print(5 ** 2)
-print(3 ** 3)
+# print(5 ** 2)
+# print(3 ** 3)
+
+# TODO: Assignment Operator '='
+
+a = 4
+
+# TODO: short hand notation
+# a = a + 2
+# a += 2
+
+# a = a * 3
+# a *= 3
+# print(a)
+
+
+# TODO: Comparison Operator(Relational operator)
+# It returns boolean value True/False
+'''
+> : Greater than
+< : Less than
+>= : Greater than equal to
+<= : Less than equal to
+!= : not equal to
+== : equal to
+'''
+
+# print( 5 > 3)
+# print(5 >= 3)
+# print(2>=4)
+# print(5<=5)
+# print(2 != 3)
+
+# print(2 == 2)
+# NOTE: NA
+# print(a = 2)
+
+# NOTE: '==' this is relational operator , '=' assignment operator
+
+# TODO: Logical operator
+'''
+and : If anyone i/p is False o/p will be False
+
+or : If anyone i/p is True o/p will be True
+
+not: It reverses the state
+'''
+
+# print (5>3 and 2<1 and 5>3)
+# print (5>3 or 2<1 or 5>3)
+# print ((5>3 and 2<1) or 5>2)
+
+# print(not 0)
+# print(not None)
+# print(not -2)
+
+# TODO: membership operator (in)
+# nums = [1,2,3,4,5]
+# print(11 in nums) 
+# print(11 not in nums)
+
+# name = "John"
+# print("J" in name)
+
+# TODO: identity operator (is)
+
