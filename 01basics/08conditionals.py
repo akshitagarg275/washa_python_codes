14c7ebc0c04719fbbb2cff8c47670842772e950f
akshita garg <akshitagarg275@gmail.com>
2023-03-05 00:51:47 +0530

    condtionals , loops

---
+++ 01basics/08conditionals.py
@@ -0,0 +1,109 @@
+'''
+Conditionals:
+Decision Statements: It helps in making decision
+
+-only if
+-if else pair
+-if elif... else
+'''
+
+# age = int(input("enter your age: "))
+
+# if age >= 18:
+#     print("We are in if block")
+#     print("You can get a license")
+  
+
+# if age >= 18:
+#     print("We are in if block")
+#     print("You can get a license")
+# else:
+#     print("You cannot get a license")
+
+# TODO: need to check different conditions
+
+# num = int(input("Enter a num: "))
+
+# if num > 0:
+#     print("Num is +ve")
+# elif num == 0:
+#     print("0")
+# elif num < 0:
+#     print("It is a -ve")
+
+# if num > 0:
+#     print("Num is +ve")
+# if num == 0:
+#     print("0")
+# if num < 0:
+#     print("It is a -ve")
+# else:
+#     print("It is else block")
+
+# TODO: nested if-else
+# find greatest among three nums
+
+# num1 = int(input("Enter a num1: "))
+# num2 = int(input("Enter a num2: "))
+# num3 = int(input("Enter a num3: "))
+
+# if num1 >= num2 :
+#     if num1 >= num3:
+#         print("num1 is greatest")
+#     else:
+#         print("num3 is greatest")
+# else:
+#     if num2 >= num3:
+#         print("num2 is greatest")
+#     else:
+#         print("num3 is greatest")
+
+# if num1 >= num2 and num1 >= num3 :
+#     print("num 1 is greatest")
+# elif num2 >= num1 and num2 >= num3 :
+#     print("num 2 is greatest")
+# elif num3 >= num1 and num3 >= num2 :
+#     print("num 3 is greatest")
+
+# TODO: login system
+# isEmail = True
+# isGoogle = False
+# isFacebook = False
+
+# if isEmail:
+#     print("You are logged in")
+# elif isGoogle:
+#     print("You are logged in")
+# elif isFacebook:
+#     print("You are logged in")
+# else:
+#     print("Please login")
+
+# if isEmail or isGoogle or isFacebook:
+#     print("You are logged in")
+# else:
+#     print("Please login")
+
+
+# TODO: Payment system
+isLogin = True
+isCart = False
+isCard = True
+
+# if isLogin:
+#     if isCart:
+#         if isCard:
+#             print("Order is placed")
+#         else:
+#             print("Check card details")
+#     else:
+#         print("Add item in the cart")
+# else:
+#     print("Please login")
+
+# if isLogin and isCart and isCard:
+#     print("Order placed")
+# else:
+#     print("Check the details")
+
+print("Other lines of code")
---
+++ 01basics/09loops.py
@@ -0,0 +1,38 @@
+'''
+Loops - > To repeat a certain task for a certain number of times
+
+while loop
+- initialization
+- condition
+- increment/decrement (re-initialization)
+
+
+for loop -> It is smart loop
+It is used on iterables (range, list, tuple, dictionary , strings)
+
+'''
+
+# i = 0
+
+# while i < 5:
+#     print("Hello : ", i)
+#     i = i + 1
+
+# num = 5
+
+# while num > 0:
+#     print(num)
+#     num = num - 1
+
+
+# for in loop
+# for i in range(1,6):
+#     print(i)
+
+nums = [12, 13, 14, 15, 16]
+
+for n in nums:
+    print(n)
+
+# print("Other lines of Code")
+
